032161e091dcdfa1de7e077ea749c6bec5e95076
Carlos Diones <carlosdiones28@gmail.com>
2022-07-18 22:43:52 -0300

    Using Linq

--- Program.cs
+++ Program.cs
@@ -1,18 +1,46 @@
 ﻿using System;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq.
 
 namespace csharp_playground
 {
     class Program
     {
-       static async Task Main(string[] args)
+       static void Main(string[] args)
        {
-           Console.WriteLine("Dicionário de Língua Portuguesa\n-----------------------------------------------------------");
-           Console.WriteLine("Escreva uma palavra para buscar sua definição");
-           Console.Write(">> ");
-           string word = Console.ReadLine();
-           
-           await PortugueseDictionary.GetDefinition(word.ToLower());
+         var startingDeck = from s in Suits()
+                            from r in Ranks()
+                            select new { Suit = s, Rank = r };
+        foreach (var card in startingDeck)
+        {
+            Console.WriteLine(card);
+        }
+
+       }
+
+       static IEnumerable<string> Suits()
+       {
+           yield return "clubs";
+           yield return "diamonds";
+           yield return "hearts";
+           yield return "spades";
+       }
+
+       static IEnumerable<string> Ranks()
+       {
+           yield return "two";
+           yield return "three";
+           yield return "four";
+           yield return "five";
+           yield return "six";
+           yield return "seven";
+           yield return "eight";
+           yield return "nine";
+           yield return "ten";
+           yield return "jack";
+           yield return "queen";
+           yield return "king";
+           yield return "ace";
        }
     }
    
